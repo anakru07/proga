815f11b082a61299763e47bbeab7c5f050d4f3d0
anakru07 <44068643+anakru07@users.noreply.github.com>
2019-10-13 18:59:43 +0300

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -21,39 +21,43 @@ class RequestHandler(http.BaseHTTPRequestHandler):
                                encoding='UTF-8'))
 
     def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
         form = cgi.FieldStorage(self.rfile, self.headers, environ={'REQUEST_METHOD': 'POST'})
         query = str(form.getvalue('query'))
 
         indexing = search.indexer.Indexer('database')
-        with open('text.txt', 'w') as test_file_1:
-            test_file_1.write('The class . 10 English paper .was conducted from 10:30 am to 01')
 
         indexing.indexing_with_lines('text.txt')
         searching = search.SearchEngine('database')
-        result = searching.search_to_sentence(query, 6)
+        result = searching.search_to_context(query)
+        print(result)
         del searching
 
         request_val = self.get_list(result)
-        self.wfile.write(bytes(f'''
+        self.wfile.write(bytes('''
             <html>
                 <body>
                     <form method = 'post'>
-                        <input type = 'text' name = 'query' value = {query}>
+                        <input type = 'text' name = 'query' value = '{0}.format(query)>
                         <input type = 'submit'>
                     </form>
                 </body>
             </html>
             ''',
                                encoding='UTF-8'))
+
         self.wfile.write(bytes(request_val, encoding='UTF-8'))
+
 
     @staticmethod
     def get_list(dict_to_htmllist):
         answer_ord_list = '<ol> '
         for cur_file in dict_to_htmllist:
-            answer_ord_list += '<li> <p> ' + cur_file + ' </p> </li> '
+            answer_ord_list += '<li> <p> ' + cur_file + ' </p> </li> ' + '<ul>'
             for cur_cit in dict_to_htmllist[cur_file]:
-                answer_ord_list += '<ul> <li> <p> ' + cur_cit.line + ' </p> </li> '
+                answer_ord_list += '<li> <p> ' + str(cur_cit) + ' </p> </li> '
             answer_ord_list += '</ul> '
         answer_ord_list += '</ol>'
         print(answer_ord_list)
